91873ee7ab3075cfde2ab9ac929045977c8b5625
Jack Zheng <jackzheng@nyu.edu>
2019-03-13 19:16:04 -0400

    Border changes color to the player who wins

--- hw02/NYUCodebase/main.cpp
+++ hw02/NYUCodebase/main.cpp
@@ -5,6 +5,8 @@
 #define GL_GLEXT_PROTOTYPES 1
 #include <SDL_opengl.h>
 #include <SDL_image.h>
+#include <chrono>
+#include <thread>
 
 #ifdef _WINDOWS
 #define RESOURCE_FOLDER ""
@@ -22,13 +24,16 @@ class Entity {
 public:
 	void draw(ShaderProgram &p);
 	void move(float elapsed);
+	void setColor(float r, float g, float b, float a);
+
+	int textureID;
 	float x;
 	float y;
-	float rotation = 0.0f;
-	int textureID;
-
 	float width;
 	float height;
+	float r, g, b, a;
+
+	float rotation = 0.0f;
 	float velocity = 1.0f;
 	float direction_x = 0.0f;
 	float direction_y = 0.0f;
@@ -43,6 +48,7 @@ void Entity::draw(ShaderProgram &program) {
 	modelMatrix = glm::translate(modelMatrix, glm::vec3(x, y, 0.0f));
 	modelMatrix = glm::scale(modelMatrix, glm::vec3(width, height, 1.0f));
 	program.SetModelMatrix(modelMatrix);
+	program.SetColor(r, g, b, a);
 
 	glDrawArrays(GL_TRIANGLES, 0, 6);
 	glDisableVertexAttribArray(program.positionAttribute);
@@ -53,20 +59,41 @@ void Entity::move(float elapsed) {
 	this->y = this->y + (this->direction_y * elapsed * this->velocity);
 }
 
+void Entity::setColor(float r, float g, float b, float a) {
+	this->r = r;
+	this->g = g;
+	this->b = b;
+	this->a = a;
+}
+
 bool collision(Entity &a, Entity &b, float space) {
-	float distance_between_x = abs(a.x - b.x) - ((a.width + b.width) / 2) - space;
-	float distance_between_y = abs(a.y - b.y) - ((a.height + b.height) / 2) - space;
-	if (distance_between_x < 0.0f && distance_between_y < 0.0f) {
+	float distance_between_x = abs(a.x - b.x) - ((a.width + b.width) / 2);
+	float distance_between_y = abs(a.y - b.y) - ((a.height + b.height) / 2);
+	if (distance_between_x < space && distance_between_y < space) {
 		return true;
 	}
 	return false;
 }
 
 bool outOfBound(Entity &ball) {
-	if (abs(ball.x) > 1.78) {
+	if (abs(ball.x) > 2.0f) {
 		return true;
 	}
 	return false;
+}
+
+void resetGame(Entity &leftPaddle, Entity &rightPaddle, Entity &ball) {
+	leftPaddle.x = -1.72f;
+	leftPaddle.y = 0.0f;
+
+	rightPaddle.x = 1.72f;
+	rightPaddle.y = 0.0f;
+
+	ball.x = 0.0f;
+	ball.y = 0.0f;
+	ball.direction_x = 0.0f;
+	ball.direction_y = 0.0f;
+	ball.velocity = 1.0f;
 }
 
 SDL_Window* displayWindow;
@@ -74,6 +101,9 @@ SDL_GLContext context;
 ShaderProgram program;
 const Uint8 *keys;
 bool done = false;
+bool restart = false;
+float game_end;
+float counter = 0.0f;
 glm::mat4 projectionMatrix, viewMatrix;
 Entity leftPaddle, rightPaddle, ball, topBar, bottomBar;
 float lastFrameTicks = 0.0f;
@@ -95,28 +125,35 @@ void Setup() {
 	leftPaddle.y = 0.0f;
 	leftPaddle.width = 0.04f;
 	leftPaddle.height = 0.33f;
+	leftPaddle.setColor(255.0f, 0.0f, 0.0f, 1.0f);
 
 	rightPaddle.x = 1.72f;
 	rightPaddle.y = 0.0f;
 	rightPaddle.width = 0.04f;
 	rightPaddle.height = 0.33f;
+	rightPaddle.setColor(0.0f, 0.0f, 255.0f, 1.0f);
 
 	ball.x = 0.0f;
 	ball.y = 0.0f;
 	ball.width = 0.05f;
 	ball.height = 0.05f;
-	ball.direction_x = 0.9f;
-	ball.direction_y = 0.5f;
+	ball.setColor(255.0f, 255.0f, 255.0f, 1.0f);
+	int rand_x = rand() % 10;
+	ball.direction_x = 1.0f ? -1.0f : rand_x >= 4;
+	int rand_y = rand() % 13 - 6;
+	ball.direction_y = rand_y / 10.0f;
 
 	topBar.x = 0.0f;
 	topBar.y = 0.95f;
-	topBar.width = 3.5f;
+	topBar.width = 3.75f;
 	topBar.height = 0.04f;
+	topBar.setColor(255.0f, 255.0f, 255.0f, 1.0f);
 
 	bottomBar.x = 0.0f;
 	bottomBar.y = -0.95f;
-	bottomBar.width = 3.5f;
+	bottomBar.width = 3.75f;
 	bottomBar.height = 0.04f;
+	bottomBar.setColor(255.0f, 255.0f, 255.0f, 1.0f);
 
 	projectionMatrix = glm::mat4(1.0f);
 	projectionMatrix = glm::ortho(-1.777f, 1.777f, -1.0f, 1.0f, -1.0f, 1.0f);
@@ -139,30 +176,43 @@ void ProcessEvents() {
 	}
 
 	if (keys[SDL_SCANCODE_UP]) {
-		rightPaddle.direction_y = 1.0f;
+		rightPaddle.direction_y = 1.5f;
 	} else if (keys[SDL_SCANCODE_DOWN]) {
-		rightPaddle.direction_y = -1.0f;
+		rightPaddle.direction_y = -1.5f;
 	} else {
 		rightPaddle.direction_y = 0.0f;
 	}
 
 	if (keys[SDL_SCANCODE_W]) {
-		leftPaddle.direction_y = 1.0f;
+		leftPaddle.direction_y = 1.5f;
 	} else if (keys[SDL_SCANCODE_S]) {
-		leftPaddle.direction_y = -1.0f;
+		leftPaddle.direction_y = -1.5f;
 	} else {
 		leftPaddle.direction_y = 0.0f;
 	}
 }
 
 void Update() {
-	float ticks = (float)SDL_GetTicks() / 1000.0f;
+	float ticks = (float) SDL_GetTicks() / 1000.0f;
 	float elapsed = ticks - lastFrameTicks;
 	lastFrameTicks = ticks;
+
+	if (ball.velocity <= 1.5f) {
+		ball.velocity = (float) (1000.0f + counter) / 1000.0f;
+		counter = counter + 1.0f;
+	}
 
 	leftPaddle.move(elapsed);
 	rightPaddle.move(elapsed);
 	ball.move(elapsed);
+
+	if (restart && (abs(lastFrameTicks - game_end) > 2.0f)) {
+		int rand_x = rand() % 10;
+		ball.direction_x = 1.0f ? -1.0f : rand_x >= 4;
+		int rand_y = rand() % 13 - 6;
+		ball.direction_y = rand_y / 10.0f;
+		restart = false;
+	}
 
 	// Check collision
 	if (collision(leftPaddle, topBar, 0.02f) || collision(leftPaddle, bottomBar, 0.02f)) {
@@ -173,18 +223,34 @@ void Update() {
 		rightPaddle.move(-1.0f * elapsed);
 	}
 
-	if (collision(ball, leftPaddle, 0.0f) || collision(ball, rightPaddle, 0.0f)) {
-		ball.direction_x = -ball.direction_x;
+	if (collision(ball, topBar, 0.005f) || collision(ball, bottomBar, 0.005f)) {
+		ball.direction_y = -ball.direction_y;
 	}
 
-	if (outOfBound(ball)) {
+	if (collision(ball, leftPaddle, 0.005f) || collision(ball, rightPaddle, 0.005f)) {
 		ball.direction_x = -ball.direction_x;
+		if (collision(ball, leftPaddle, 0.005f)) {
+			float distance_from_center = ball.y - leftPaddle.y;
+			ball.direction_y = distance_from_center / (leftPaddle.height / 2) * 0.6f;
+		} else if (collision(ball, rightPaddle, 0.005f)){
+			float distance_from_center = ball.y - rightPaddle.y;
+			ball.direction_y = distance_from_center / (rightPaddle.height / 2) * 0.6f;
+		}
 	}
 
 	// Check winning conditions
-	// Check if blocks reach border
-	if (collision(ball, topBar, 0.0f) || collision(ball, bottomBar, 0.0f)) {
-		ball.direction_y = 0 - ball.direction_y;
+	if (outOfBound(ball)) {
+		if (ball.x > 0) {
+			topBar.setColor(255.0f, 0.0f, 0.0f, 1.0f);
+			bottomBar.setColor(255.0f, 0.0f, 0.0f, 1.0f);
+		} else {
+			topBar.setColor(0.0f, 0.0f, 255.0f, 1.0f);
+			bottomBar.setColor(0.0f, 0.0f, 255.0f, 1.0f);
+		}
+		resetGame(leftPaddle, rightPaddle, ball);
+		game_end = lastFrameTicks;
+		counter = 0.0f;
+		restart = true;
 	}
 }
 
